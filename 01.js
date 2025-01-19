a2fa575017b998e7a619e6897a78372ca932692a
happycoder-soft <happykumar1632@gmail.com>
2025-01-19 23:04:49 +0530

    practice

--- 01.js
+++ 01.js
@@ -1,6 +1,62 @@
-console.log("kaal hack")
-let a=20;
-let b=60;
-console.log(a+b)
+// console.log("kaal hack")
+// let a=20;
+// let b=60;
+// console.log(a+b)
 
-console.log("50"+5)
+// console.log("50"+5)
+
+//change data type to another data type
+
+let str="happy"
+console.log(typeof str);
+let num1=Number(str)
+console.log(num1);
+
+let num2=505
+let str2=String(num2)
+console.log(typeof str2);
+
+// operations
+let num3=-100      
+let op=-num3           
+let opp= +num3
+console.log(op);
+console.log(opp);
+
+console.log("1"+2+3);
+console.log(5+6+"2")
+
+//prefix postfix operation
+let num5=200
+++num5;
+console.log(num5);
+
+let num6=200
+--num6;
+console.log(num6);
+
+//comprison
+console.log(2==2);
+console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
+
+// array , object , function
+//array
+const list=["game" , "squide game" , "money heist"]
+console.log(typeof list);
+
+//object
+let new_login={
+    name: "happy",
+    age : 20,
+    state: "bihar"
+}
+console.log(new_login);
+
+//function
+
+const new_fun=function(){
+    console.log("JAVA SCRIPT PRACTICE TIME")
+}
+new_fun()  //call function
